664982ce03cba8fefb7289128d6e1cf7fbe675f9
vkaware <vaibhavkaware2@gmail.com>
2022-07-21 23:59:17 +0530

    day3

--- scripts/support.js
+++ scripts/support.js
@@ -3,7 +3,7 @@ let input = document.querySelector("input");
 input.addEventListener("keyup",(e)=>{
     if(e.keyCode === 13 && (e.target.value === "time tracking")){
        //console.log(e.target.value)
-       window.location.href="./timeTracking.html"
+       window.location.href="./TimeTracking.html"
     }
     else if(e.keyCode === 13 && (e.target.value === "project tracking")){
       window.location.href="./projectTracking.html"
