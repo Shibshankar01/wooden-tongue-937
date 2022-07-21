44dd347c245e07a13e749656b944c1bc262862da
bArSu45 <barsaraniojha@gmal.com>
2022-07-22 01:18:36 +0530

    updated

---
+++ scripts/index.js
@@ -0,0 +1,351 @@
+
+
+const onClickDiv1 = document.querySelector('#onClickDiv1');
+const onClickDiv2 = document.querySelector('#onClickDiv2');
+const onClickDiv3 = document.querySelector('#onClickDiv3');
+const onClickDiv4 = document.querySelector('#onClickDiv4');
+const onClickDiv5 = document.querySelector('#onClickDiv5');
+const onClickDiv6 = document.querySelector('#onClickDiv6');
+
+const ri8_box_1 = document.querySelector('#ri8_box_1');
+
+
+let clickFunc1 = () => {
+
+    onClickDiv1.style.backgroundColor = "#e7edfc"
+   
+    onClickDiv2.style.backgroundColor = "white"
+    onClickDiv3.style.backgroundColor = "white"
+    onClickDiv4.style.backgroundColor = "white"
+    onClickDiv5.style.backgroundColor = "white"
+    onClickDiv6.style.backgroundColor = "white"
+
+    onClickDiv1.style.borderColor = "#3070f0"
+    onClickDiv2.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv3.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv4.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv5.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv6.style.borderColor = "rgb(186, 185, 185)"
+
+
+    ri8_box_1.innerHTML = ` <img id="img_2" src="https://tmetric.com/media/e5enfaae/img-advantages.png" alt="">`
+
+
+    onClickDiv1.innerHTML = `<div class="div_1"><img class="img_4" src="https://tmetric.com/media/2p4n4oyc/icon-timer-blue.svg"
+    alt="">
+<h2>Time Tracking</h2>
+</div>
+<p>Capture every task you work on. Take control over time intervals by simple time tracking</p>`
+
+    onClickDiv2.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/1niddz51/icon-project-blue.svg" alt="">
+    <h2>Project Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv3.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/2gxihhdu/icon-team-blue.svg" alt="">
+    <h2>Team Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv4.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/s4koycnp/icon-invoice-blue.svg" alt="">
+    <h2>
+        Billing & Invoicing</h2>
+</div>
+<p></p>`
+
+    onClickDiv5.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/yoblhiut/icon-reports-blue.svg" alt="">
+    <h2>Reporting</h2>
+</div>
+<p></p>`
+
+    onClickDiv6.innerHTML = ` <div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/cajpa4uy/icon-time-off-blue.svg" alt="">
+    <h2>Time Off</h2>
+</div>
+<p></p>`
+
+}
+let clickFunc2 = () => {
+
+    onClickDiv1.style.backgroundColor = "white"
+    onClickDiv2.style.backgroundColor = "#e7edfc"
+    onClickDiv3.style.backgroundColor = "white"
+    onClickDiv4.style.backgroundColor = "white"
+    onClickDiv5.style.backgroundColor = "white"
+    onClickDiv6.style.backgroundColor = "white"
+
+    onClickDiv1.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv2.style.borderColor = "#3070f0"
+    onClickDiv3.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv4.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv5.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv6.style.borderColor = "rgb(186, 185, 185)"
+
+    ri8_box_1.innerHTML = ` <img id="img_2" src="https://tmetric.com/media/d2dhexfq/img-advantages-project-management.png" alt="">`
+
+    // `https://tmetric.com/media/d2dhexfq/img-advantages-project-management.png`
+
+    onClickDiv1.innerHTML = `<div class="div_1"><img class="img_4" src="https://tmetric.com/media/2p4n4oyc/icon-timer-blue.svg"
+    alt="">
+<h2>Time Tracking</h2>
+</div>
+<p></p>`
+
+    onClickDiv2.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/1niddz51/icon-project-blue.svg" alt="">
+    <h2>Project Management</h2>
+</div>
+<p>Create projects, set rates and budgets. Keep tracking of hours spent on tasks and see the project progress at a glance, collaborate easily, keep deadlines and prevent over budgeting</p>`
+
+    onClickDiv3.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/2gxihhdu/icon-team-blue.svg" alt="">
+    <h2>Team Management</h2>
+</div>
+<p></p>`
+
+
+    onClickDiv4.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/s4koycnp/icon-invoice-blue.svg" alt="">
+    <h2>
+        Billing & Invoicing</h2>
+</div>
+<p></p>`
+
+    onClickDiv5.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/yoblhiut/icon-reports-blue.svg" alt="">
+    <h2>Reporting</h2>
+</div>
+<p></p>`
+
+    onClickDiv6.innerHTML = ` <div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/cajpa4uy/icon-time-off-blue.svg" alt="">
+    <h2>Time Off</h2>
+</div>
+<p></p>`
+}
+let clickFunc3 = () => {
+
+    onClickDiv1.style.backgroundColor = "white"
+    onClickDiv2.style.backgroundColor = "white"
+    onClickDiv3.style.backgroundColor = "#e7edfc"
+    onClickDiv4.style.backgroundColor = "white"
+    onClickDiv5.style.backgroundColor = "white"
+    onClickDiv6.style.backgroundColor = "white"
+
+    onClickDiv1.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv2.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv3.style.borderColor = "#3070f0"
+    onClickDiv4.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv5.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv6.style.borderColor = "rgb(186, 185, 185)"
+
+    ri8_box_1.innerHTML = ` <img id="img_2" src="https://tmetric.com/media/b3abexjt/img-advantages-team-management.png" alt="">`
+
+    onClickDiv1.innerHTML = `<div class="div_1"><img class="img_4" src="https://tmetric.com/media/2p4n4oyc/icon-timer-blue.svg"
+    alt="">
+<h2>Time Tracking</h2>
+</div>
+<p</p>`
+
+    onClickDiv2.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/1niddz51/icon-project-blue.svg" alt="">
+    <h2>Project Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv3.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/2gxihhdu/icon-team-blue.svg" alt="">
+    <h2>Team Management</h2>
+</div>
+<p>With the accurate billing system, set billable rates and easily create an invoice based on tracked time and expenses, and get paid promptly.</p>`
+
+    onClickDiv4.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/s4koycnp/icon-invoice-blue.svg" alt="">
+    <h2>
+        Billing & Invoicing</h2>
+</div>
+<p></p>`
+
+    onClickDiv5.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/yoblhiut/icon-reports-blue.svg" alt="">
+    <h2>Reporting</h2>
+</div>
+<p></p>`
+
+    onClickDiv6.innerHTML = ` <div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/cajpa4uy/icon-time-off-blue.svg" alt="">
+    <h2>Time Off</h2>
+</div>
+<p></p>`
+}
+let clickFunc4 = () => {
+
+    onClickDiv1.style.backgroundColor = "white"
+    onClickDiv2.style.backgroundColor = "white"
+    onClickDiv3.style.backgroundColor = "white"
+    onClickDiv4.style.backgroundColor = "#e7edfc"
+    onClickDiv5.style.backgroundColor = "white"
+    onClickDiv6.style.backgroundColor = "white"
+
+    onClickDiv1.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv2.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv3.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv4.style.borderColor = "#3070f0"
+    onClickDiv5.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv6.style.borderColor = "rgb(186, 185, 185)"
+
+    ri8_box_1.innerHTML = ` <img id="img_2" src="https://tmetric.com/media/nmmhl5l3/img-advantages-invoice.png" alt="">`
+
+    onClickDiv1.innerHTML = `<div class="div_1"><img class="img_4" src="https://tmetric.com/media/2p4n4oyc/icon-timer-blue.svg"
+    alt="">
+<h2>Time Tracking</h2>
+</div>
+<p></p>`
+
+    onClickDiv2.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/1niddz51/icon-project-blue.svg" alt="">
+    <h2>Project Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv3.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/2gxihhdu/icon-team-blue.svg" alt="">
+    <h2>Team Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv4.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/s4koycnp/icon-invoice-blue.svg" alt="">
+    <h2>
+        Billing & Invoicing</h2>
+</div>
+<p>With the accurate billing system, set billable rates and easily create an invoice based on tracked time and expenses, and get paid promptly.</p>`
+
+    onClickDiv5.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/yoblhiut/icon-reports-blue.svg" alt="">
+    <h2>Reporting</h2>
+</div>
+<p></p>`
+
+    onClickDiv6.innerHTML = ` <div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/cajpa4uy/icon-time-off-blue.svg" alt="">
+    <h2>Time Off</h2>
+</div>
+<p></p>`
+}
+
+let clickFunc5 = () => {
+
+    onClickDiv1.style.backgroundColor = "white"
+    onClickDiv2.style.backgroundColor = "white"
+    onClickDiv3.style.backgroundColor = "white"
+    onClickDiv4.style.backgroundColor = "white"
+    onClickDiv5.style.backgroundColor = "#e7edfc"
+    onClickDiv6.style.backgroundColor = "white"
+
+    onClickDiv1.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv2.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv3.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv4.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv5.style.borderColor = "#3070f0"
+    onClickDiv6.style.borderColor = "rgb(186, 185, 185)"
+
+    ri8_box_1.innerHTML = ` <img id="img_2" src="https://tmetric.com/media/bpli5hts/img-advantages-reporting.png" alt="">`
+
+    onClickDiv1.innerHTML = `<div class="div_1"><img class="img_4" src="https://tmetric.com/media/2p4n4oyc/icon-timer-blue.svg"
+    alt="">
+<h2>Time Tracking</h2>
+</div>
+<p></p>`
+
+    onClickDiv2.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/1niddz51/icon-project-blue.svg" alt="">
+    <h2>Project Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv3.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/2gxihhdu/icon-team-blue.svg" alt="">
+    <h2>Team Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv4.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/s4koycnp/icon-invoice-blue.svg" alt="">
+    <h2>
+        Billing & Invoicing</h2>
+</div>
+<p></p>`
+
+    onClickDiv5.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/yoblhiut/icon-reports-blue.svg" alt="">
+    <h2>Reporting</h2>
+</div>
+<p>With a flexible reporting system, you have a choice to get insights into your projects and team productivity, analyze incomes and keep costs under control</p>`
+
+    onClickDiv6.innerHTML = ` <div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/cajpa4uy/icon-time-off-blue.svg" alt="">
+    <h2>Time Off</h2>
+</div>
+<p></p>`
+}
+let clickFunc6 = () => {
+
+    onClickDiv1.style.backgroundColor = "white"
+    onClickDiv2.style.backgroundColor = "white"
+    onClickDiv3.style.backgroundColor = "white"
+    onClickDiv4.style.backgroundColor = "white"
+    onClickDiv5.style.backgroundColor = "white"
+    onClickDiv6.style.backgroundColor = "#e7edfc"
+
+    onClickDiv1.style.borderColor = "#rgb(186, 185, 185)"
+    onClickDiv2.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv3.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv4.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv5.style.borderColor = "rgb(186, 185, 185)"
+    onClickDiv6.style.borderColor = "#3070f0"
+
+    ri8_box_1.innerHTML = ` <img id="img_2" src="https://tmetric.com/media/kuqjgrfl/img-advantages-time-off.png" alt="">`
+
+    onClickDiv1.innerHTML = `<div class="div_1"><img class="img_4" src="https://tmetric.com/media/2p4n4oyc/icon-timer-blue.svg"
+    alt="">
+<h2>Time Tracking</h2>
+</div>
+<p></p>`
+
+    onClickDiv2.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/1niddz51/icon-project-blue.svg" alt="">
+    <h2>Project Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv3.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/2gxihhdu/icon-team-blue.svg" alt="">
+    <h2>Team Management</h2>
+</div>
+<p></p>`
+
+    onClickDiv4.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/s4koycnp/icon-invoice-blue.svg" alt="">
+    <h2>
+        Billing & Invoicing</h2>
+</div>
+<p></p>`
+
+    onClickDiv5.innerHTML = `<div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/yoblhiut/icon-reports-blue.svg" alt="">
+    <h2>Reporting</h2>
+</div>
+<p></p>`
+
+    onClickDiv6.innerHTML = ` <div class="div_1">
+    <img class="img_4" src="https://tmetric.com/media/cajpa4uy/icon-time-off-blue.svg" alt="">
+    <h2>Time Off</h2>
+</div>
+<p>Add a PTO policy for your organization, simply ask for days off, control missed workdays, manage balances and teams attendance hassle-free</p>`
+}
+
