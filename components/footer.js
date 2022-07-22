a9764865b81cd9fe0c0e62d15b51da720c07545a
Alok Kumar <alokk0018@gmail.com>
2022-07-22 22:04:42 +0530

    navbar footer added to components

---
+++ components/footer.js
@@ -0,0 +1,74 @@
+let footer = () =>{
+    return `<div class="footer-container">
+    <div class="footer">
+        <div class="footer-heading col-1">
+            <a href=""><img src="https://tmetric.com/images/tmetric_logo_with_text.svg" width="147" height="54"
+                    alt=""></a>
+        </div>
+        <div class="footer-heading col-2">
+            <h4>Applications</h4>
+            <!-- <i class="fa-solid fa-desktop"></i> -->
+            <a href="https://tmetric.com/download"><span><i class="fa-solid fa-tv"></i></span>Desktop</a>
+            <a href="https://apps.apple.com/us/app/tmetric/id1251407402"><span><i
+                        class="fab fa-apple fa-lg"></i></span> iOS</a>
+            <a href="https://play.google.com/store/apps/details?id=com.tmetric"><span><i
+                        class="fab fa-android fa-lg"></i></span>Android</a>
+        </div>
+        <div class="footer-heading col-3">
+            <h4>Browser Extensions</h4>
+            <a
+                href="https://chrome.google.com/webstore/detail/tmetric-%E2%80%93-time-tracker-pr/ffijoclmniipjbhecddgkfpdafpbdnen">
+                <span><i class="fab fa-chrome fa-lg"></i></span>Chrome
+            </a>
+            <a href="https://addons.mozilla.org/en-US/firefox/addon/tmetric-extension/">
+                <span><i class="fab fa-firefox fa-lg"></i></span>Firefox
+            </a>
+            <a href="https://addons.opera.com/en/extensions/details/tmetric-extension/">
+                <span><i class="fab fa-opera fa-lg"></i></span>Opera
+            </a>
+            <a
+                href="https://microsoftedge.microsoft.com/addons/detail/tmetric-%E2%80%93-time-tracker-/bkohhohbfioiffcejghnjljadblbifok">
+                <span><i class="fab fa-edge fa-lg"></i></span>Edge
+            </a>
+            <a href="https://apps.apple.com/app/tmetric-for-safari/id1483939427">
+                <span><i class="fab fa-safari fa-lg"></i></span>Safari
+            </a>
+        </div>
+        <div class="footer-heading col-4">
+            <h4>Resources</h4>
+            <a href="solutions.html">Solutions</a>
+            <a href="integrations.html">Integrations</a>
+            <a href="https://tmetric.com/help">Help</a>
+            <a href="https://tmetric.com/time-trackers-comparison">Time Trackers Comparison</a>
+            <a href="blog.html">Blog</a>
+        </div>
+        <div class="footer-heading col-5">
+            <h4>About</h4>
+            <a href="https://tmetric.com/about">About TMetric</a>
+            <a href="https://tmetric.com/terms-of-service">Terms of Service</a>
+            <a href="https://tmetric.com/privacy-policy">Privacy Policy</a>
+            <a href="https://tmetric.com/cookies-policy">Cookies Policy</a>
+        </div>
+    </div>
+    <hr id="navhr">
+    <div id="copyright">Copyright © Devart 2022
+        <div>
+            <a href="https://www.facebook.com/tmetrictimer/">
+                <i class="fab fa-facebook fa-lg"></i>
+            </a>
+            <a href="https://twitter.com/TMetric_Timer">
+                <i class="fab fa-twitter fa-lg"></i>
+            </a>
+            <a
+                href="https://www.linkedin.com/showcase/tmetric/?original_referer=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2F&originalSubdomain=cz">
+                <i class="fab fa-linkedin fa-lg"></i>
+            </a>
+            <a href="https://www.youtube.com/channel/UCPmbWbH2xUKdmVwVcQMOxaw">
+                <i class="fab fa-youtube fa-lg"></i>
+            </a>
+        </div>
+    </div>
+</div>`
+}
+
+export default footer;
---
+++ components/navbar.js
@@ -0,0 +1,20 @@
+let navbar = () =>{
+ return `<div id="navbar">
+ <div>
+     <a href=""><img src="https://tmetric.com/images/tmetric_logo_with_text.svg" id="navLogo" alt=""></a>
+     <button id="language">EN</button>
+ </div>
+ <div>
+     <div class="nav_menu drop-down">Why TMetric <i class="fa fa-angle-down"></i></div>
+     <div class="nav_menu drop-down">Apps & Integrations <i class="fa fa-angle-down"></i></div>
+     <div class="nav_menu"><a href="pricing.html">Pricing</a></div>
+     <div class="nav_menu"><a href="support.html">Support</a></div>
+     <div class="nav_menu"><a href="blog.html">Blog</a></div>
+ </div>
+ <div>
+     <a href="login.html" id="nav_login">Log In</a>
+     <a href="signup.html" id="nav_signin">Sign Up</a>
+ </div>
+</div>`
+}
+export default navbar;
